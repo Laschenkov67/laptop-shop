798be3b77138876561d3f68a1c0d177034e47677
Artem <Artem@DESKTOP-JJ9O5GM>
2020-08-06 17:10:02 +0300

    routing user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ProductPageComponent,
+    CartPageComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
